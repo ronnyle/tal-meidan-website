4859e98d7063476ad4e29ead610ae4df0fe4c201
ronny.lehmann <ronny.lehmann@servicenow.com>
2024-10-13 16:02:19 -0700

    crap

---
+++ README.md
@@ -0,0 +1,1 @@
+# tal-meidan-website
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./_includes/**/*.html", "./_layouts/**/*.html", "./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
